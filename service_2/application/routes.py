f1962fb6f612844e22684656c81b24cdd0b8a100
abaseen-popal <abaseen.p@gmail.com>
2021-01-20 15:22:07 +0000

    Adding the feature of service 2

---
+++ service_2/application/routes.py
@@ -0,0 +1,10 @@
+from application import app
+from flask import request, Response
+import random 
+
+@app.route("/pokemon",methods=["GET"])
+def get_pokemon():
+    pokemon = ["Pikachu - Yellow (Lightning)", "Charizard - Red (Fire)", "Mudkip - Blue (Water)","Venusaur - Green (Grass)"]
+    return Response(str(random.choice(pokemon)), mimetype='text/plain')
+
+
