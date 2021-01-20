8fe415156715a7a3beb255b6ac84da5547153792
abaseen-popal <abaseen.p@gmail.com>
2021-01-20 15:27:27 +0000

    Adding the feature of service 3

---
+++ service_3/application/routes.py
@@ -0,0 +1,10 @@
+from application import app
+from flask import request, Response
+import random 
+
+@app.route("/psa", methods=["GET"])
+def get_psa ():
+    psa  = ["10- Gem Mint - Pristine", "9 - Mint",  "8- Near Mint-Mint",
+     "7- Near Mint", "6- Excellent- Near Mint", "5- Excellent","4- Very Good - Excellent","3 - Very Good",
+     "2- good", "1- Poor"]
+    return Response(str(random.choice(psa)), mimetype='text/plain')
