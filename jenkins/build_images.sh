e807980bfe93d56fa20fddc33f1ba1f139d5afff
abaseen-popal <abaseen.p@gmail.com>
2021-01-21 15:38:40 +0000

    Made changes to jenkins file to test in development branch

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -9,7 +9,7 @@ sudo su - jenkins
 sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-set pass {mypassword}
+set pass ${"DOCKPW"}
 
 docker login --username={dockerhub_username} --email={dockerhub_email}
 
