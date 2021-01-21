7c38f691317f751be3ce9b1bf89cee33243fcee6
abaseen-popal <abaseen.p@gmail.com>
2021-01-21 22:29:42 +0000

    testing the docker-compose build p4

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -9,7 +9,7 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker jenkins
 sudo su - jenkins
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # set pass ${"DOCKPW"}
