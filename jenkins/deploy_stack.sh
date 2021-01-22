32d499bba652e6eeae564b62fcf94a70f40d4506
abaseen-popal <abaseen.p@gmail.com>
2021-01-22 14:44:57 +0000

    Creating deploy p1

---
+++ jenkins/deploy_stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scp -i ~/.shh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
+    export DATABSE_URI=${DATABSE_URI}
+    export AUTHOR=${AUTHOR}
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml pokemon-stack
+EOF
