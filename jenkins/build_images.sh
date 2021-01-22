c4a9e38e47bafd1c0021036815ee49b861aa9016
abaseen-popal <abaseen.p@gmail.com>
2021-01-22 11:10:12 +0000

    Removing slashes in sh

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -6,23 +6,14 @@ sudo apt install -y curl jq
 
 curl https://get.docker.com | sudo bash
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker jenkins
 sudo su jenkins
-
-
-# set pass ${"DOCKPW"}
 
 docker login
 
 docker-compose up -d
 
 docker-compose push 
-
-
-# expect "password: "
-# send "$pass"
-# expect "password: "
-# send "$pass"
