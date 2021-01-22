c1166dddc3837e424f4eec0476181a819d29097a
abaseen-popal <abaseen.p@gmail.com>
2021-01-22 12:32:53 +0000

    adding the shabang

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,7 +5,6 @@ sudo apt-get install -y python3-venv python3-pip
 
 # Test service 1
 cd service_1
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
@@ -13,7 +12,6 @@ cd ..
 
 # Test service 2
 cd service_2
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
@@ -21,7 +19,6 @@ cd ..
 
 # Test service 3
 cd service_3
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
@@ -29,7 +26,6 @@ cd ..
 
 # Test service 4
 cd service_4
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
