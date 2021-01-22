8d8fad80cfe7bc820d965bc3ab2f6f128995028b
abaseen-popal <abaseen.p@gmail.com>
2021-01-22 12:38:00 +0000

    making changes

--- jenkins/ansible_init.sh
+++ jenkins/ansible_init.sh
@@ -3,4 +3,9 @@
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
+sudo apt install python3
+sudo apt install python3-pip
+
 pip3 install --user ansible
+
+cd ansible && /home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
