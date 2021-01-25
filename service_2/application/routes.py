8838aeb5e5634db6740ee43e1494c1423f3222e7
abaseen-popal <73040060+abaseen-popal@users.noreply.github.com>
2021-01-25 01:30:09 +0000

    Revert "Poke changes (#4)"
    This reverts commit 6960ccc7b873455ef8f8367c053dad190b0edc6c.

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -5,7 +5,8 @@ import random
 
 @app.route("/pokemon",methods=["GET"])
 def get_pokemon():
-    pokemon = ["Pikachu - Yellow (Lightning)", "Charizard - Red (Fire)", "Mudkip - Blue (Water)","Venusaur - Green (Grass)", "Gengar - Purple (Ghost)"]
+    # adding the pokemon into a list 
+    pokemon = ["Pikachu - Yellow (Lightning)", "Charizard - Red (Fire)", "Mudkip - Blue (Water)","Venusaur - Green (Grass)"]
     return Response(str(random.choice(pokemon)), mimetype='text/plain')
     # returning the value as a string which a random choice from the list
 
