6c775b1a85ec892dbad32ddf81047d8573c39971
abaseen-popal <73040060+abaseen-popal@users.noreply.github.com>
2021-01-25 02:01:47 +0000

    Revert "Poke changes (#6)"
    This reverts commit f854a9f96c8148cde137f6888f2789428dea1f14.

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -6,6 +6,6 @@ import random
 def get_psa ():
     psa  = ["10- Gem Mint - Pristine", "9 - Mint",  "8- Near Mint-Mint",
      "7- Near Mint", "6- Excellent- Near Mint", "5- Excellent","4- Very Good - Excellent","3 - Very Good",
-     "2- good", "1- Poor", "0- Destroyed"]
+     "2- good", "1- Poor"]
     return Response(str(random.choice(psa)), mimetype='text/plain')
     # The value is returned as a string 
