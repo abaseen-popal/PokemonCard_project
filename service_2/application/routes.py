e0b5d1965c6f04d0809f1a1823a091190a9f46a5
abaseen-popal <73040060+abaseen-popal@users.noreply.github.com>
2021-01-25 11:05:46 +0000

    Poke changes (#12)
    * made changes to the service for rolling update
    
    * pushing everything to poke-changes
    
    * making changes

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -5,8 +5,7 @@ import random
 
 @app.route("/pokemon",methods=["GET"])
 def get_pokemon():
-    # adding the pokemon into a list 
-    pokemon = ["Pikachu - Yellow (Lightning)", "Charizard - Red (Fire)", "Mudkip - Blue (Water)","Venusaur - Green (Grass)"]
+    pokemon = ["Pikachu - Yellow (Lightning)", "Charizard - Red (Fire)", "Mudkip - Blue (Water)","Venusaur - Green (Grass)", "Gengar - Purple (Ghost)"]
     return Response(str(random.choice(pokemon)), mimetype='text/plain')
     # returning the value as a string which a random choice from the list
 
